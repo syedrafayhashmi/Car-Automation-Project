f198cea8536dd826c383f2ceb854fd85befa91d9
syedrafayhashmi <rafaysidsid@gmail.com>
2019-12-15 21:59:24 +0500

    start button

---
+++ CAR_new/CAR_new.ino
@@ -0,0 +1,50 @@
+const int RELAY1 = 15;  //for door lock relay 1 and 2
+const int RELAY2 = 2;
+const int RELAY3 = 0;  //for car self
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+   pinMode(RELAY1, OUTPUT);
+  digitalWrite(RELAY1  ,HIGH);
+  pinMode(RELAY2, OUTPUT);
+  digitalWrite(RELAY2  ,LOW);
+ }
+
+void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+   int ip =  SerialBT.read();
+   
+  //ALL DOORS
+  if (ip == 1)
+  {digitalWrite(RELAY1, HIGH);
+   digitalWrite(RELAY2, LOW);
+   
+  }  
+  if (ip == 0)
+  {digitalWrite(RELAY1, LOW);
+   digitalWrite(RELAY2, HIGH);
+   
+  } 
+  if (ip > 10)
+   {del = ip;} 
+  if (ip == 2)
+  {   
+    digitalWrite(RELAY3,HIGH);
+    delay(del);
+    digitalWrite(RELAY3,LOW);
+    }
+}
+delay(20);
+}
